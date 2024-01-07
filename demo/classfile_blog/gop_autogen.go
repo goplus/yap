23136e4b22eede79462bf08fcc0eb1820f647a81
xushiwei <x@goplus.org>
2024-01-08 02:46:11 +0800

    blog.yapx.gox => blog_yap.gox

---
+++ demo/classfile_blog/gop_autogen.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/goplus/yap"
+
+type blog_yap struct {
+	yap.App
+}
+//line demo/classfile_blog/blog_yap.gox:1
+func (this *blog_yap) MainEntry() {
+//line demo/classfile_blog/blog_yap.gox:1:1
+	this.Get("/p/:id", func(ctx *yap.Context) {
+//line demo/classfile_blog/blog_yap.gox:2:1
+		ctx.Yap__1("article", map[string]string{"id": ctx.Param("id")})
+	})
+//line demo/classfile_blog/blog_yap.gox:7:1
+	this.Run__1(":8080")
+}
+func main() {
+	yap.Gopt_App_Main(new(blog_yap))
+}
