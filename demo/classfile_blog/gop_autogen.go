cad67d6921667422654b84046ce225636337d573
xushiwei <x@goplus.org>
2024-01-08 23:51:53 +0800

    demo go.mod

--- demo/classfile_blog/gop_autogen.go
+++ demo/classfile_blog/gop_autogen.go
@@ -5,14 +5,14 @@ import "github.com/goplus/yap"
 type blog struct {
 	yap.App
 }
-//line demo/classfile_blog/blog_yap.gox:1
+//line classfile_blog/blog_yap.gox:1
 func (this *blog) MainEntry() {
-//line demo/classfile_blog/blog_yap.gox:1:1
+//line classfile_blog/blog_yap.gox:1:1
 	this.Get("/p/:id", func(ctx *yap.Context) {
-//line demo/classfile_blog/blog_yap.gox:2:1
+//line classfile_blog/blog_yap.gox:2:1
 		ctx.Yap__1("article", map[string]string{"id": ctx.Param("id")})
 	})
-//line demo/classfile_blog/blog_yap.gox:7:1
+//line classfile_blog/blog_yap.gox:7:1
 	this.Run__1(":8080")
 }
 func main() {
--- demo/classfile_hello/gop_autogen.go
+++ demo/classfile_hello/gop_autogen.go
@@ -5,19 +5,19 @@ import "github.com/goplus/yap"
 type hello struct {
 	yap.App
 }
-//line demo/classfile_hello/hello_yap.gox:1
+//line classfile_hello/hello_yap.gox:1
 func (this *hello) MainEntry() {
-//line demo/classfile_hello/hello_yap.gox:1:1
+//line classfile_hello/hello_yap.gox:1:1
 	this.Get("/p/:id", func(ctx *yap.Context) {
-//line demo/classfile_hello/hello_yap.gox:2:1
+//line classfile_hello/hello_yap.gox:2:1
 		ctx.Json__1(map[string]string{"id": ctx.Param("id")})
 	})
-//line demo/classfile_hello/hello_yap.gox:6:1
+//line classfile_hello/hello_yap.gox:6:1
 	this.Handle("/", func(ctx *yap.Context) {
-//line demo/classfile_hello/hello_yap.gox:7:1
+//line classfile_hello/hello_yap.gox:7:1
 		ctx.Html__1(`<html><body>Hello, <a href="/p/123">Yap</a>!</body></html>`)
 	})
-//line demo/classfile_hello/hello_yap.gox:10:1
+//line classfile_hello/hello_yap.gox:10:1
 	this.Run__1(":8080")
 }
 func main() {
