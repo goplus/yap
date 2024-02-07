1670749aa8f8e907158856e5859dd29bab2f2d24
xushiwei <x@goplus.org>
2024-02-08 04:41:31 +0800

    use mysql, sqlite3 in same project

--- ydb/demo/foo/gop_autogen.go
+++ ydb/demo/foo/gop_autogen.go
@@ -15,16 +15,6 @@ import (
 
 const _ = true
 
-type User struct {
-	Id       string `id CHAR(32) UNIQUE`
-	Spwd     string
-	Salt     string
-	Nickname string
-	Email    string    `CHAR(64) INDEX`
-	Tel      string    `CHAR(16) INDEX`
-	Born     time.Time `INDEX`
-	Ctime    time.Time `DATETIME(6) INDEX`
-}
 type ArticleEntry struct {
 	Id     string `CHAR(32) UNIQUE`
 	Author string `CHAR(24) INDEX`
@@ -39,13 +29,67 @@ type Tag struct {
 	Name    string `CHAR(24) UNIQUE(article)`
 	Article string `CHAR(32)`
 }
-type foo struct {
+type User struct {
+	Id       string `id CHAR(32) UNIQUE`
+	Spwd     string
+	Salt     string
+	Nickname string
+	Email    string    `CHAR(64) INDEX`
+	Tel      string    `CHAR(16) INDEX`
+	Born     time.Time `INDEX`
+	Ctime    time.Time `DATETIME(6) INDEX`
+}
+type article struct {
+	ydb.Sql
+}
+type user struct {
 	ydb.Sql
 }
 
 func main() {
-//line ydb/demo/foo/foo_ydb.gox:82:1
-	ydb.Gopt_AppGen_Main(new(ydb.AppGen), new(foo))
+//line ydb/demo/foo/user_ydb.gox:23:1
+	ydb.Gopt_AppGen_Main(new(ydb.AppGen), new(article), new(user))
+}
+//line ydb/demo/foo/article_ydb.gox:20
+func (this *article) Main() {
+//line ydb/demo/foo/article_ydb.gox:20:1
+	this.Engine__0("mysql")
+//line ydb/demo/foo/article_ydb.gox:22:1
+	ydb.Gopt_Sql_Gopx_Table[Article](this, "v0.1.0")
+//line ydb/demo/foo/article_ydb.gox:23:1
+	this.From("oldart v0.9.1", func() {
+	})
+//line ydb/demo/foo/article_ydb.gox:28:1
+	ydb.Gopt_Sql_Gopx_Table[Tag](this, "v0.1.0")
+//line ydb/demo/foo/article_ydb.gox:30:1
+	this.Class("Articles", func() {
+//line ydb/demo/foo/article_ydb.gox:31:1
+		this.Use("article")
+//line ydb/demo/foo/article_ydb.gox:33:1
+		this.Api("listByTag", func(tag string) (result []ArticleEntry) {
+//line ydb/demo/foo/article_ydb.gox:34:1
+			var ids []string
+//line ydb/demo/foo/article_ydb.gox:35:1
+			this.Query__1("tag.name=?", tag)
+//line ydb/demo/foo/article_ydb.gox:36:1
+			this.Ret__1("tag.article", &ids)
+//line ydb/demo/foo/article_ydb.gox:38:1
+			this.Query__1("id=?", ids)
+//line ydb/demo/foo/article_ydb.gox:39:1
+			this.Ret__1(&result)
+//line ydb/demo/foo/article_ydb.gox:40:1
+			return
+		})
+//line ydb/demo/foo/article_ydb.gox:43:1
+		this.Api("listByAuthor", func(author string) (result []ArticleEntry) {
+//line ydb/demo/foo/article_ydb.gox:44:1
+			this.Query__1("author=?", author)
+//line ydb/demo/foo/article_ydb.gox:45:1
+			this.Ret__1(&result)
+//line ydb/demo/foo/article_ydb.gox:46:1
+			return
+		})
+	})
 }
 
 var ErrNoEmailAndTel = errors.New("no email and telephone")
@@ -62,102 +106,66 @@ func Hs256(pwd string, salt string) string {
 //line ydb/demo/foo/foo.gop:25:1
 	return base64.RawURLEncoding.EncodeToString(b)
 }
-//line ydb/demo/foo/foo_ydb.gox:36
-func (this *foo) Main() {
-//line ydb/demo/foo/foo_ydb.gox:36:1
-	this.Engine__0("mysql")
-//line ydb/demo/foo/foo_ydb.gox:38:1
+//line ydb/demo/foo/user_ydb.gox:19
+func (this *user) Main() {
+//line ydb/demo/foo/user_ydb.gox:19:1
+	this.Engine__0("sqlite3")
+//line ydb/demo/foo/user_ydb.gox:21:1
 	ydb.Gopt_Sql_Gopx_Table[User](this, "user v0.1.0")
-//line ydb/demo/foo/foo_ydb.gox:40:1
-	ydb.Gopt_Sql_Gopx_Table[Article](this, "v0.1.0")
-//line ydb/demo/foo/foo_ydb.gox:41:1
-	this.From("oldart v0.9.1", func() {
-	})
-//line ydb/demo/foo/foo_ydb.gox:46:1
-	ydb.Gopt_Sql_Gopx_Table[Tag](this, "v0.1.0")
-//line ydb/demo/foo/foo_ydb.gox:48:1
+//line ydb/demo/foo/user_ydb.gox:23:1
 	this.Class("Users", func() {
-//line ydb/demo/foo/foo_ydb.gox:49:1
+//line ydb/demo/foo/user_ydb.gox:24:1
 		this.Use("user")
-//line ydb/demo/foo/foo_ydb.gox:51:1
+//line ydb/demo/foo/user_ydb.gox:26:1
 		this.Api("register", func(id string, pwd string, nickname string, email string, tel string, ctime time.Time) error {
-//line ydb/demo/foo/foo_ydb.gox:52:1
+//line ydb/demo/foo/user_ydb.gox:27:1
 			if email == "" && tel == "" {
-//line ydb/demo/foo/foo_ydb.gox:53:1
+//line ydb/demo/foo/user_ydb.gox:28:1
 				return ErrNoEmailAndTel
 			}
-//line ydb/demo/foo/foo_ydb.gox:55:1
+//line ydb/demo/foo/user_ydb.gox:30:1
 			this.Limit__2(3, "email=?", email)
-//line ydb/demo/foo/foo_ydb.gox:56:1
+//line ydb/demo/foo/user_ydb.gox:31:1
 			this.Limit__2(3, "tel=?", tel)
-//line ydb/demo/foo/foo_ydb.gox:58:1
+//line ydb/demo/foo/user_ydb.gox:33:1
 			salt := Rand()
-//line ydb/demo/foo/foo_ydb.gox:59:1
+//line ydb/demo/foo/user_ydb.gox:34:1
 			spwd := Hs256(pwd, salt)
-//line ydb/demo/foo/foo_ydb.gox:60:1
+//line ydb/demo/foo/user_ydb.gox:35:1
 			this.Insert__1(&User{Id: id, Spwd: spwd, Salt: salt, Nickname: nickname, Email: email, Tel: tel, Ctime: ctime})
-//line ydb/demo/foo/foo_ydb.gox:61:1
+//line ydb/demo/foo/user_ydb.gox:36:1
 			return nil
 		})
-//line ydb/demo/foo/foo_ydb.gox:63:1
+//line ydb/demo/foo/user_ydb.gox:38:1
 		this.Call__1("user", "pwd", "nickname", "", "", time.Now())
-//line ydb/demo/foo/foo_ydb.gox:64:1
+//line ydb/demo/foo/user_ydb.gox:39:1
 		this.Ret__1(ErrNoEmailAndTel)
-//line ydb/demo/foo/foo_ydb.gox:65:1
+//line ydb/demo/foo/user_ydb.gox:40:1
 		this.Call__1("user", "pwd", "nickname", "user@foo.com", "", time.Now())
-//line ydb/demo/foo/foo_ydb.gox:66:1
+//line ydb/demo/foo/user_ydb.gox:41:1
 		this.Ret__0(nil)
-//line ydb/demo/foo/foo_ydb.gox:67:1
+//line ydb/demo/foo/user_ydb.gox:42:1
 		this.Call__1("user", "pwd", "nickname", "user@foo.com", "13500000000", time.Now())
-//line ydb/demo/foo/foo_ydb.gox:68:1
+//line ydb/demo/foo/user_ydb.gox:43:1
 		this.Ret__1(ydb.ErrDuplicated)
-//line ydb/demo/foo/foo_ydb.gox:70:1
+//line ydb/demo/foo/user_ydb.gox:45:1
 		this.Api("login", func(id string, pwd string) bool {
-//line ydb/demo/foo/foo_ydb.gox:71:1
+//line ydb/demo/foo/user_ydb.gox:46:1
 			var spwd, salt string
-//line ydb/demo/foo/foo_ydb.gox:72:1
+//line ydb/demo/foo/user_ydb.gox:47:1
 			this.Query__1("id=?", id)
-//line ydb/demo/foo/foo_ydb.gox:73:1
+//line ydb/demo/foo/user_ydb.gox:48:1
 			this.Ret__1("salt", &salt, "spwd", &spwd)
-//line ydb/demo/foo/foo_ydb.gox:74:1
+//line ydb/demo/foo/user_ydb.gox:49:1
 			return Hs256(pwd, salt) == spwd
 		})
-//line ydb/demo/foo/foo_ydb.gox:76:1
+//line ydb/demo/foo/user_ydb.gox:51:1
 		this.Call__1("", "")
-//line ydb/demo/foo/foo_ydb.gox:77:1
+//line ydb/demo/foo/user_ydb.gox:52:1
 		this.Ret__1(false)
-//line ydb/demo/foo/foo_ydb.gox:78:1
+//line ydb/demo/foo/user_ydb.gox:53:1
 		this.Call__1("user", "pwd")
-//line ydb/demo/foo/foo_ydb.gox:79:1
+//line ydb/demo/foo/user_ydb.gox:54:1
 		this.Ret__1(true)
-	})
-//line ydb/demo/foo/foo_ydb.gox:82:1
-	this.Class("Articles", func() {
-//line ydb/demo/foo/foo_ydb.gox:83:1
-		this.Use("article")
-//line ydb/demo/foo/foo_ydb.gox:85:1
-		this.Api("listByTag", func(tag string) (result []ArticleEntry) {
-//line ydb/demo/foo/foo_ydb.gox:86:1
-			var ids []string
-//line ydb/demo/foo/foo_ydb.gox:87:1
-			this.Query__1("tag.name=?", tag)
-//line ydb/demo/foo/foo_ydb.gox:88:1
-			this.Ret__1("tag.article", &ids)
-//line ydb/demo/foo/foo_ydb.gox:90:1
-			this.Query__1("id=?", ids)
-//line ydb/demo/foo/foo_ydb.gox:91:1
-			this.Ret__1(&result)
-//line ydb/demo/foo/foo_ydb.gox:92:1
-			return
-		})
-//line ydb/demo/foo/foo_ydb.gox:95:1
-		this.Api("listByAuthor", func(author string) (result []ArticleEntry) {
-//line ydb/demo/foo/foo_ydb.gox:96:1
-			this.Query__1("author=?", author)
-//line ydb/demo/foo/foo_ydb.gox:97:1
-			this.Ret__1(&result)
-//line ydb/demo/foo/foo_ydb.gox:98:1
-			return
-		})
 	})
 }
