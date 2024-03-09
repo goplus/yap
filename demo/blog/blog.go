f0c7f248a9d95bea521a12b8a4250d461d5973df
xushiwei <x@goplus.org>
2024-03-09 11:45:42 +0800

    yap: Go demo

--- demo/blog/blog.go
+++ demo/blog/blog.go
@@ -9,6 +9,9 @@ import (
 func main() {
 	y := yap.New(os.DirFS("."))
 
+	y.GET("/", func(ctx *yap.Context) {
+		ctx.TEXT(200, "text/html", `<html><body>Hello, <a href="/p/123">YAP</a>!</body></html>`)
+	})
 	y.GET("/p/:id", func(ctx *yap.Context) {
 		ctx.YAP(200, "article", yap.H{
 			"id": ctx.Param("id"),
