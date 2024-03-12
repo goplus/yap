c4af9b8b7caa1e40cb4e27c772c5208822589f12
LiusCraft <liuscraft@qq.com>
2024-03-12 09:48:41 +0800

    test: add demo

---
+++ demo/blog_nestetemplate/blog.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/goplus/yap"
+)
+
+func main() {
+	y := yap.New(os.DirFS("."))
+
+	y.GET("/", func(ctx *yap.Context) {
+		ctx.TEXT(200, "text/html", `<html><body>Hello, <a href="/p/123">YAP</a>!</body></html>`)
+	})
+	y.GET("/p/:id", func(ctx *yap.Context) {
+		ctx.YAP(200, "blog", yap.H{
+			"Id": ctx.Param("id"),
+		})
+	})
+	y.Run(":8888")
+}
